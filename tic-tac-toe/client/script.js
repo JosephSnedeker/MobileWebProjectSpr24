2ce9866f0547ea46f4361f7758634278a65fdf9a
Zach Chanza <zchanza@uccs.edu>
2024-04-19 21:22:25 -0600

    online functional2

--- tic-tac-toe/client/script.js
+++ tic-tac-toe/client/script.js
@@ -129,7 +129,7 @@ function isGameOver() {
         for(i=2;i<9;i=i+3){
             btn[i].style.color = "red"; 
         }
-        return true
+        return true;
     } 
     else if (((b[4] == 'X') && (b[5] == 'X') && (b[6] == 'X')) ||
              ((b[4] == 'O') && (b[5] == 'O') && (b[6] == 'O'))) { 
@@ -150,7 +150,7 @@ function isGameOver() {
     }
 }
 
-function GameTide() {
+function GameTied() {
     if ((b[1] == 'X' || b[1] == 'O') && (b[2] == 'X'
         || b[2] == 'O') && (b[3] == 'X' || b[3] == 'O') && 
         (b[4] == 'X' || b[4] == 'O') && (b[5] == 'X' || 
